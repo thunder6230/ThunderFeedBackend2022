6be6211b9869786f2322d2268b7d04d00fcfb645
thunder6230 <62564512+thunder6230@users.noreply.github.com>
2022-05-23 19:34:56 +0200

    migrations

---
+++ Migrations/20220523133702_addCreatedAtToCommentsTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    public partial class addCreatedAtToCommentsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Comments",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Comments");
+
+            migrationBuilder.CreateTable(
+                name: "LikeForPost",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    UserPostId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LikeForPost", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LikeForPost_UserPosts_UserPostId",
+                        column: x => x.UserPostId,
+                        principalTable: "UserPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikeForPost_UserPostId",
+                table: "LikeForPost",
+                column: "UserPostId");
+        }
+    }
+}
